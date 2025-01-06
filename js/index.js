d1ab00742a1c9a8218e827d94b822d414f9c31ac
Almadhi07 <aaalma05@louisville.edu>
2025-01-06 18:58:23 -0500

    Tested the JS operators & implemented txt file to answer the questions

--- js/index.js
+++ js/index.js
@@ -2,3 +2,18 @@
 // that are not declared
 "use strict"
 
+/*
+age = 23;
+console.log(age + 1);
+*/
+
+let age = 23;
+
+if(age >= 23){
+    console.log("You are old enough!")
+
+}
+
+else(age <=23)
+    console.log("You are too young!")
+
